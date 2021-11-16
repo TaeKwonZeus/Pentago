c71c6ecab08b21d8e601d358548dd2a1364e3bf2
nikinov <novellenicholas@gmail.com>
2021-11-16 19:42:27 +0100

    engine test

---
+++ engine/engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace engine
+{
+    public class engine
+    {
+        public engine()
+        {
+            
+        }
+
+        public void GetBoardInfo()
+        {
+            
+        }
+
+        public void MakeMove()
+        {
+            
+        }
+    }
+}
