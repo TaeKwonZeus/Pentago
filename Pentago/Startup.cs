bfab7a35e1697cbb13d6735f8205d4716428906f
baka da yo <dyatlovleonid@gmail.com>
2021-11-23 20:41:49 +0300

    added lowercase URLs

--- Pentago/Startup.cs
+++ Pentago/Startup.cs
@@ -28,6 +28,7 @@ namespace Pentago
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddRouting(options => { options.LowercaseUrls = true; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
