96982e7c899e55ab9aa8b196913b5b7bafe01e34
baka da yo <dyatlovleonid@gmail.com>
2021-12-05 23:06:35 +0300

    ee

--- Pentago.API/Startup.cs
+++ Pentago.API/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -48,6 +49,33 @@ namespace Pentago.API
                 .AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            var connection = new SqliteConnection(Configuration.GetConnectionString("App"));
+            connection.Open();
+
+            new SqliteCommand(@"CREATE TABLE IF NOT EXISTS users
+                (
+                    id                  INTEGER PRIMARY KEY ASC,
+                    username            VARCHAR(32)        NOT NULL,
+                    normalized_username VARCHAR(32) UNIQUE NOT NULL,
+                    email               VARCHAR(32) UNIQUE NOT NULL,
+                    password_hash       CHAR(64)           NOT NULL,
+                    api_key_hash        CHAR(64),
+                    glicko_rating       INT                NOT NULL,
+                    glicko_rd           REAL               NOT NULL
+                );
+
+                CREATE TABLE IF NOT EXISTS games
+                (
+                    id        INTEGER PRIMARY KEY ASC,
+                    white     INT NOT NULL,
+                    black     INT NOT NULL,
+                    game_data TEXT,
+                    FOREIGN KEY (white) REFERENCES users (id),
+                    FOREIGN KEY (black) REFERENCES users (id)
+                );", connection).ExecuteNonQuery();
+
+            connection.Close();
         }
     }
 }
